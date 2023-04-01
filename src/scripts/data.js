2a2a5c410c746f29bbc00edbe1da6176fad0df22
ningxiao <dsgrichard@hotmail.com>
2023-03-31 17:53:49 -0700

    draw diagram

--- src/scripts/data.js
+++ src/scripts/data.js
@@ -20,7 +20,7 @@
 // };
 export const fetchData = async (e) => { 
     // e.preventDefault();
-    const stateNum = "06" //or "*"this will be change as state changed currently set as California
+    const stateNum = "*" //or "*"this will be change as state changed currently set as California
     const countyNum = "*" //or "*"
     const censusUrl = `https://api.census.gov/data/2020/acs/flows?get=FULL1_NAME,STATE2,STATE2_NAME,FULL2_NAME,MOVEDNET&for=county:${countyNum}&in=state:${stateNum}`
    
@@ -31,28 +31,29 @@ export const fetchData = async (e) => {
         if (res.ok){
             let data = await res.json();
             
-            const currentStateArray = data[1][0].split(", ")
-            const currentState = (currentStateArray[currentStateArray.length-1])
+            
             const migrationCount = {}
             console.log(data[5])
+            data.filter(ele => ele[2] === "00")
             data.slice(1).forEach(ele =>{
-                
-                const migrateStateArray = ele[1].split(", ")
-                let migrateDes = (migrateStateArray[migrateStateArray.length - 1])
+                const currentStateArray = ele[0].split(", ")
+                const currentState = (currentStateArray[currentStateArray.length - 1])
+                let migrateDes = ele[2]
                 // console.log(migrateDes);
                 // if (migrateDes === currentState){
                 //     migrateDes = ele[1]
                 // }
-                if (!ele[2]){
-                    ele[2] = 0;
+                if (!ele[4]) {
+                    ele[4] = 0;
                 }
-                if (!migrationCount[[ele[0], migrateDes]]) {
-                    migrationCount[[ele[0], migrateDes]] = 0
+                if (!migrationCount[currentState, migrateDes]) {
+                    migrationCount[currentState, migrateDes] = 0
                 }
-                migrationCount[[ele[0], migrateDes]] += parseInt(ele[2]);
+                migrationCount[currentState, migrateDes] += parseInt(ele[4]);
                 
             })
             console.log(migrationCount)
+            console.log(migrationCount[1])
         }else{
             throw new Error(res);
         }
@@ -61,3 +62,4 @@ export const fetchData = async (e) => {
     }
 }
 
+export {migrationCount};
