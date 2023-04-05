4f484bf58f9cb12c14a478c91964bd82778e0bb5
ningxiao <dsgrichard@hotmail.com>
2023-04-05 16:38:18 -0700

    update

--- src/index.js
+++ src/index.js
@@ -1,19 +1,30 @@
 import {fetchData} from "./scripts/data.js";
+import { fetchStateData } from "./scripts/data.js";
 import {drawDiagram} from "./scripts/diagram.js"; 
 import {getAllName} from "./scripts/names";
 import { handleSelection } from "./scripts/utils.js";
 import { statesArray } from "./scripts/states.js";
 
+
+
+
 // fetchData("2020", "*", "*", statesArray).then(matrix => drawDiagram(statesArray, matrix))
+fetchStateData().then(matrix => drawDiagram(2020, statesArray, matrix));
 
 document.getElementById("submit-button").addEventListener("click",function(event){
     event.preventDefault();
+    document.getElementById("loading-container").style.display = "block"
     const svgContainer = document.getElementById('diagram-container');
+    
     const firstSvg = svgContainer.querySelector('svg');
     if (firstSvg) {
         svgContainer.removeChild(firstSvg);
     }
+    
     handleSelection();
+    // if (firstSvg) {
+    //     loadingDiv.style.display = "none";
+    // }
 })
 
 
